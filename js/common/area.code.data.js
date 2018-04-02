0bfa3e1d848cf9cab1df8bc14d85de1da5cc9d0c
maomao <15614633703@163.com>
2018-04-02 17:22:53 +0800

    init

---
+++ js/common/area.code.data.js
@@ -0,0 +1,29 @@
+var areaData = [{
+		value: 'CN',
+		text: '中国--0086'
+	},
+	{
+		value: 'US',
+		text: '美国--001'
+	},
+	{
+		value: 'US',
+		text: '加拿大--001'
+	},
+	{
+		value: 'US',
+		text: '澳大利亚--0061'
+	},
+	{
+		value: 'US',
+		text: '英国--0044'
+	},
+	{
+		value: 'US',
+		text: '法国--0033'
+	},
+	{
+		value: 'US',
+		text: '日本--0081'
+	}
+]
